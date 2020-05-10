25c99f572a50e984744c527417c3df4aa7e7fc49
Eduard Spack <edi.spack@gmail.com>
2020-05-10 22:58:37 +0300

    Initial commit.

---
+++ Conway's Game of Life/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Conway_s_Game_of_Life
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.stepLabel = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.OnTick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 56);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.OnStart);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(94, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 56);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "STOP";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.OnStop);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(175, 13);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 56);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "NEXT";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.OnNext);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.BackColor = System.Drawing.SystemColors.Control;
+            this.trackBar1.Location = new System.Drawing.Point(337, 13);
+            this.trackBar1.Maximum = 20;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(400, 56);
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.Value = 2;
+            this.trackBar1.Scroll += new System.EventHandler(this.OnSpeedChange);
+            // 
+            // stepLabel
+            // 
+            this.stepLabel.BackColor = System.Drawing.SystemColors.Control;
+            this.stepLabel.Location = new System.Drawing.Point(743, 13);
+            this.stepLabel.Name = "stepLabel";
+            this.stepLabel.Size = new System.Drawing.Size(180, 56);
+            this.stepLabel.TabIndex = 4;
+            this.stepLabel.Text = "STOPPED";
+            this.stepLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(256, 13);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 56);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "CLEAR";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.OnClear);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 673);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.stepLabel);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Conway\'s Game of Life";
+            this.Load += new System.EventHandler(this.OnLoad);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.OnPaint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.OnClick);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label stepLabel;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Conway's Game of Life/Form1.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Conway_s_Game_of_Life
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static int o = 7, l = 15;
+        int w, h, mw, mh, state, step, ok = 0;
+        bool[,] mat, matv;
+        Bitmap bm;
+        Graphics g;
+
+        private void OnLoad(object sender, EventArgs e)
+        {
+            w = this.Width;
+            h = this.Height;
+            mw = w / l + 2 * o;
+            mh = h / l + 2 * o;
+
+            bm = new Bitmap(w, h);
+            mat = new bool[mw, mh];
+            matv = new bool[mw, mh];
+
+            Init();
+        }
+
+        private void OnPaint(object sender, PaintEventArgs e)
+        {
+            if(ok == 0)
+            {
+                g = this.CreateGraphics();
+                Redraw();
+                if (state == 0)
+                {
+                    stepLabel.Text = "STOPPED, STEP " + step;
+                }
+                else
+                {
+                    stepLabel.Text = "RUNNING, STEP " + step;
+                }
+                ok = 1;
+            }
+        }
+
+        private void OnTick(object sender, EventArgs e)
+        {
+            int i, j;
+            NextStep();
+            for(i = 0; i < mw; i++)
+            {
+                for(j = 0; j < mh; j++)
+                {
+                    if (mat[i, j] != matv[i, j])
+                    {
+                        if (i - o >= 0 && i + o < mw && j - o >= 0 && j + o < mh)
+                        {
+                            SetCellInBmp(i, j, mat[i, j]);
+                        }
+                    }
+                }
+            }
+            Redraw();
+            step++;
+            stepLabel.Text = "RUNNING, STEP " + step;
+        }
+
+        private void OnClick(object sender, MouseEventArgs e)
+        {
+            int x, y, i, j;
+            if(state == 0)
+            {
+                x = e.X;
+                y = e.Y;
+                i = o + x / l;
+                j = o + y / l;
+                mat[i, j] = !mat[i, j];
+                SetCellInBmp(i, j, mat[i, j]);
+                Redraw();
+            }
+        }
+
+        private void OnStart(object sender, EventArgs e)
+        {
+            state = 1;
+            timer.Interval = 2000 / trackBar1.Value;
+            timer.Enabled = true;
+            stepLabel.Text = "RUNNING, STEP " + step;
+        }
+
+        private void OnStop(object sender, EventArgs e)
+        {
+            timer.Enabled = false;
+            state = 0;
+            stepLabel.Text = "STOPPED, STEP " + step;
+        }
+
+        private void OnNext(object sender, EventArgs e)
+        {
+            int i, j;
+            if(state == 0)
+            {
+                NextStep();
+                for (i = 0; i < mw; i++)
+                {
+                    for (j = 0; j < mh; j++)
+                    {
+                        if (mat[i, j] != matv[i, j])
+                        {
+                            if (i - o >= 0 && i + o < mw && j - o >= 0 && j + o < mh)
+                            {
+                                SetCellInBmp(i, j, mat[i, j]);
+                            }
+                        }
+                    }
+                }
+                Redraw();
+                step++;
+                stepLabel.Text = "STOPPED, STEP " + step;
+
+            }
+        }
+
+        private void OnClear(object sender, EventArgs e)
+        {
+            Init();
+            Redraw();
+            stepLabel.Text = "STOPPED, STEP " + step;
+        }
+
+        private void OnSpeedChange(object sender, EventArgs e)
+        {
+            timer.Interval = 2000 / trackBar1.Value;
+        }
+
+        private void Init()
+        {
+            int i, j;
+            state = 0; // 0 - stopped; 1 - running
+            step = 0;
+            timer.Enabled = false;
+
+            for (i = 0; i < mw; i++)
+            {
+                for (j = 0; j < mh; j++)
+                {
+                    mat[i, j] = false;
+                }
+            }
+
+            for (i = 0; i < w; i++)
+            {
+                for (j = 0; j < h; j++)
+                {
+                    if (j % l == 0 || (j % l != 0 && i % l == 0))
+                    {
+                        bm.SetPixel(i, j, Color.Gray);
+                    }
+                    else
+                    {
+                        bm.SetPixel(i, j, Color.White);
+                    }
+                }
+            }
+        }
+
+        private void NextStep()
+        {
+            int i, j, nr;
+
+            for (i = 0; i < mw; i++)
+            {
+                for (j = 0; j < mh; j++)
+                {
+                    matv[i, j] = mat[i, j];
+                }
+            }
+
+            for (i = 1; i < mw - 1; i++)
+            {
+                for (j = 1; j < mh - 1; j++)
+                {
+                    nr = 0;
+                    if (matv[i - 1, j - 1] == true)
+                        nr++;
+                    if (matv[i - 1, j] == true)
+                        nr++;
+                    if (matv[i - 1, j + 1] == true)
+                        nr++;
+                    if (matv[i, j - 1] == true)
+                        nr++;
+                    if (matv[i, j + 1] == true)
+                        nr++;
+                    if (matv[i + 1, j - 1] == true)
+                        nr++;
+                    if (matv[i + 1, j] == true)
+                        nr++;
+                    if (matv[i + 1, j + 1] == true)
+                        nr++;
+
+                    if(matv[i, j] == true)
+                    {
+                        if (nr == 2 || nr == 3)
+                        {
+                            mat[i, j] = true;
+                        }
+                        else
+                        {
+                            mat[i, j] = false;
+                        }
+                    }
+                    else
+                    {
+                        if (nr == 3)
+                        {
+                            mat[i, j] = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void SetCellInBmp(int x, int y, bool k)
+        {
+            int i, j;
+            for(i = 1; i < l; i++)
+            {
+                for(j = 1; j < l; j++)
+                {
+                    if(k == true)
+                    {
+                        if (x - o >= 0 && x + o < mw && y - o >= 0 && y + o < mh)
+                        {
+                            bm.SetPixel(l * (x - o) + i, l * (y - o) + j, Color.Black);
+                        }
+                    }
+                    else
+                    {
+                        if (x - o >= 0 && x + o < mw && y - o >= 0 && y + o < mh)
+                        {
+                            bm.SetPixel(l * (x - o) + i, l * (y - o) + j, Color.White);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void Redraw()
+        {
+            g.DrawImage(bm, 0, 0);
+        }
+    }
+}
